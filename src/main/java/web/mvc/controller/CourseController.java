d3bcb0a060328b2232eececec4b8fe087bd3c960
Min <jeon320@naver.com>
2025-07-01 19:08:16 +0900

    chore : 인증시 브론즈 적용, gps_verified 적용

--- src/main/java/web/mvc/controller/CourseController.java
+++ src/main/java/web/mvc/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import web.mvc.dto.ReqCustomDTO;
 import web.mvc.dto.ReqTempDTO;
 import web.mvc.dto.ResTempDTO;
+import web.mvc.security.CustomUserDetails;
 import web.mvc.service.CourseService;
 
 import java.util.List;
@@ -20,8 +21,8 @@ public class CourseController {
      * Temp 코스 검색
      */
     @GetMapping("/temp")
-    public ResponseEntity<?> searchTempCourse(/*@AuthenticationPrincipal*/@RequestParam Long id ) {
-        List<ResTempDTO> list = courseService.searchTempCourses(id);
+    public ResponseEntity<?> searchTempCourse(@AuthenticationPrincipal CustomUserDetails userDetails ) {
+        List<ResTempDTO> list = courseService.searchTempCourses(userDetails.getUser().getId());
         return ResponseEntity.ok().body(list);
     }
 
@@ -29,8 +30,8 @@ public class CourseController {
      * Temp 코스에 추가
      */
     @PostMapping("/temp")
-    public ResponseEntity<?> insertTempCourse(/*@AuthenticationPrincipal*/ @RequestParam Long id, @RequestBody ReqTempDTO course) {
-        course.setUserId(id);
+    public ResponseEntity<?> insertTempCourse(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody ReqTempDTO course) {
+        course.setUserId(userDetails.getUser().getId());
         return ResponseEntity.ok().body(courseService.insertTempCourse(course));
     }
 
@@ -38,38 +39,38 @@ public class CourseController {
      * Custom 코스 저장
      */
     @PostMapping("/custom")
-    public ResponseEntity<?> saveCustomCourse(@RequestParam Long id,@RequestBody List<ReqCustomDTO> list ) {
-        list.forEach(dto->dto.setId(id));
+    public ResponseEntity<?> saveCustomCourse(@AuthenticationPrincipal CustomUserDetails userDetails,@RequestBody List<ReqCustomDTO> list ) {
+        list.forEach(dto->dto.setId(userDetails.getUser().getId()));
         return ResponseEntity.ok(courseService.insertCustomCourse(list));
     }
     /**
      * custom 코스 상세보기
      */
-    @GetMapping("/custom/details")
-    public ResponseEntity<?> searchCustomCourseDetails(@RequestParam Long id,@RequestParam Long courseId) {
-        return ResponseEntity.ok(courseService.searchCustomCourse(id,courseId));
+    @GetMapping("/custom/details/{couseId}")
+    public ResponseEntity<?> searchCustomCourseDetails(@AuthenticationPrincipal CustomUserDetails userDetails,@PathVariable Long courseId) {
+        return ResponseEntity.ok(courseService.searchCustomCourse(userDetails.getUser().getId(),courseId));
     }
 
     /**
      * custom 코스 목록 보기
      */
     @GetMapping("/custom/list")
-    public ResponseEntity<?> searchCustomCourseList(@RequestParam Long id) {
-        return ResponseEntity.ok(courseService.searchCustomCourseList(id));
+    public ResponseEntity<?> searchCustomCourseList(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(courseService.searchCustomCourseList(userDetails.getUser().getId()));
     }
     /**
      * custom 코스 수정하기
      */
-    @PutMapping("/custom/update")
-    public void updateCustomCourse(@RequestParam Long courseId,@RequestBody List<ReqCustomDTO> list) {
+    @PutMapping("/custom/{courseId}")
+    public void updateCustomCourse(@PathVariable Long courseId,@RequestBody List<ReqCustomDTO> list) {
         courseService.updateCustomCourse(courseId,list);
 
     }
     /**
      * cutom 코스 삭제하기
      */
-    @DeleteMapping("/custom/delete")
-    public void deleteCustomCourse(@RequestParam Long courseId) {
+    @DeleteMapping("/custom/{courseId}")
+    public void deleteCustomCourse(@PathVariable Long courseId) {
         courseService.deleteCustomCourse(courseId);
     }
 }
