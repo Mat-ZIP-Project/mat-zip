06925441512d91aaaa076afd676fad71dbd25db3
Hee <wogml.hee@gmail.com>
2025-07-15 16:18:04 +0900

    fix : add log

--- src/main/java/web/mvc/config/SchedulerConfig.java
+++ src/main/java/web/mvc/config/SchedulerConfig.java
@@ -3,7 +3,7 @@ package web.mvc.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Configuration
-@EnableScheduling
+//@Configuration
+//@EnableScheduling
 public class SchedulerConfig {
 }
